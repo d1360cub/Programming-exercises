28e6f434534cdbaad12b6633233c89f3d614c82e
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-09 16:49:27 -0500

    multiply negative numbers

--- multiply.js
+++ multiply.js
@@ -1,7 +1,8 @@
 function multiply(num1, num2) {
   let result = 0;
-  for (let i = 0; i < num2; i++) {
-    result += num1;
+  let positive = Math.abs(num2) == num2;
+  for (let i = 0; i < Math.abs(num2); i++) {
+    positive ? (result += num1) : (result -= num1);
   }
   console.log(result);
 }
